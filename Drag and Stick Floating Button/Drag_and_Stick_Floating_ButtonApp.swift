86f39b3377349ca9d5cbc1ffc65de5d413933998
Swee Kwang <sweekwang94@gmail.com>
2022-12-28 22:45:42 +0800

    Added starting project

---
+++ Drag and Stick Floating Button/Drag_and_Stick_Floating_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drag_and_Stick_Floating_ButtonApp.swift
+//  Drag and Stick Floating Button
+//
+//  Created by Swee Kwang Chua on 28/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Drag_and_Stick_Floating_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
