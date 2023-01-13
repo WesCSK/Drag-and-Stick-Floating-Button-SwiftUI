bd592a11343258b5c80080d3d3b9d1b0a18bc330
Swee Kwang <sweekwang94@gmail.com>
2023-01-13 09:01:34 +0800

    Update ChatButton

--- Drag and Stick Floating Button/View/ChatButton.swift
+++ Drag and Stick Floating Button/View/ChatButton.swift
@@ -8,19 +8,35 @@
 import SwiftUI
 
 struct ChatButton: View {
+    @State var opacityState: Double = 1
+    
     var body: some View {
         Button {
-            // BUTTON ACTION
+            
         } label: {
             Circle()
                 .frame(width: 50, height: 50)
                 .foregroundColor(.white)
-                .shadow(radius: 1)
+                .shadow(radius: 3)
                 .overlay {
+
                     Image("chat")
                         .resizable()
                         .frame(width: 25, height: 25)
                 }
+        }
+        .disabled(true)
+        .opacity(opacityState)
+        .onLongPressGesture(minimumDuration: 0.2) {
+            print("press")
+            withAnimation(.linear(duration: 0.1)) {
+                opacityState = 0.2
+            }
+            
+            withAnimation(.linear(duration: 0.1).delay(0.1)) {
+                opacityState = 1
+            }
+            
         }
     }
 }
